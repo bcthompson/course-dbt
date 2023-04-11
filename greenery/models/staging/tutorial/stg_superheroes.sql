ed029c00bf8b810826b41e01b3f2457c72a90a76
Caleb Thompson <brennancthompson@gmail.com>
2023-04-11 16:07:52 +0000

    staging tutorial

---
+++ greenery/models/staging/tutorial/stg_superheroes.sql
@@ -0,0 +1,19 @@
+{{
+  config(
+    materialized='table'
+  )
+}}
+
+SELECT 
+    id AS superhero_id,
+    name,
+    gender,
+    eye_color,
+    race,
+    hair_color,
+    nullif(height, -99) as height,
+    publisher,
+    skin_color,
+    alignment,
+    nullif(weight, -99) as weight
+FROM {{ source('tutorial', 'superheroes') }}
